7d1d8b5f7f680091703a0c354d0e59a934e5daa8
Alex <alexandr.cosov@toptal.com>
2024-12-03 13:30:17 +0200

    Extending Tawilwind default theme, adding fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "hoopla-black" : "#071231",
+        "hoopla-deep-blue": "#003282",
+        "hoopla-electric-blue": "#2187FF",
+        "hoopla-bg-blueish": "#F6F9FE",
+        "hoopla-secondary-violet": "#8C7EF7",
+        "hoopla-secondary-orange": "#FA884C",
+        "hoopla-secondary-blue": "#78B6FF",
+        "hoopla-secondary-yellow": "#FAC344",
+        "hoopla-secondary-red": "#FD5A64"
+      },
+      fontFamily:{
+        "aeonikPro-regular": ["AeonikPro-Regular", "sans-serif"],
+        "aeonikPro-bold": ["AeonikPro-Bold", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
